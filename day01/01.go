d222b0ab018f576a60a7a67db9028752fc84bf13
Johan Hillerström <progr@mmer.nu>
2018-12-29 01:20:26 +0100

    Add day 1

---
+++ day01/01.go
@@ -0,0 +1,20 @@
+package day01
+
+func traverseInput(input string) (int, int) {
+	floor := 0
+	basement := -1
+	for i, x := range input {
+		switch x {
+		case '(':
+			floor++
+			break
+		case ')':
+			floor--
+			break
+		}
+		if basement == -1 && floor == -1 {
+			basement = i + 1
+		}
+	}
+	return floor, basement
+}
---
+++ day01/01_test.go
@@ -0,0 +1,46 @@
+package day01
+
+import (
+	"testing"
+
+	"github.com/hillerstorm/advent-of-code-2015-go/utils"
+)
+
+type Expect struct {
+	input  string
+	result int
+	part   utils.Part
+}
+
+func TestDayOne(t *testing.T) {
+	input := utils.ReadFile("01.in")
+
+	expects := []Expect{
+		Expect{"(())", 0, utils.Part1},
+		Expect{"()()", 0, utils.Part1},
+		Expect{"(((", 3, utils.Part1},
+		Expect{"(()(()(", 3, utils.Part1},
+		Expect{"))(((((", 3, utils.Part1},
+		Expect{"())", -1, utils.Part1},
+		Expect{"))(", -1, utils.Part1},
+		Expect{")))", -3, utils.Part1},
+		Expect{")())())", -3, utils.Part1},
+		Expect{")", 1, utils.Part2},
+		Expect{"()())", 5, utils.Part2},
+		Expect{input, 232, utils.Part1},
+		Expect{input, 1783, utils.Part2},
+	}
+
+	for i, x := range expects {
+		p1, p2 := traverseInput(x.input)
+		var res int
+		if x.part == utils.Part1 {
+			res = p1
+		} else {
+			res = p2
+		}
+		if res != x.result {
+			t.Errorf("01_p%d_t%d: Expected %d, got %d", x.part, i, x.result, res)
+		}
+	}
+}
